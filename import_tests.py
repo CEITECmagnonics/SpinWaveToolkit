241bf0102a24413353443fecf1773c7af9ccee4a
Jan Klíma <johnnyklima@gmail.com>
2024-07-10 12:02:32 +0200

    Update import_tests.py

--- import_tests.py
+++ import_tests.py
@@ -3,5 +3,7 @@ Script for testing correct imports throughout the module.
 May be removed and replaced by some automatic test (e.g. using `pytest`).
 """
 
+from pprint import pprint
 import SpinWaveToolkit as SWT
 
+pprint(dir(SWT))
