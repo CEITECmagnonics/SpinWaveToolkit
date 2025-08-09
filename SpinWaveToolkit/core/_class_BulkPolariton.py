b23db35738d310cd5450fd22c34096c53fa88bf4
Jan Klíma <johnnyklima@gmail.com>
2025-08-09 16:17:12 +0200

    Update _class_BulkPolariton.py

--- SpinWaveToolkit/core/_class_BulkPolariton.py
+++ SpinWaveToolkit/core/_class_BulkPolariton.py
@@ -1,41 +1,40 @@
 """
-Core (private) file for the `SingleLayer` class.
+Core (private) file for the `BulkPolariton` class.
 """
 
 import numpy as np
-from SpinWaveToolkit.helpers import *
+from scipy.optimize import least_squares
+from SpinWaveToolkit.helpers import MU0, C
 
 __all__ = ["BulkPolariton"]
 
 
 class BulkPolariton:
-    """
+    """Compute wave characteristic in dependance to k-vector
+    (wavenumber) such as frequency, group velocity, lifetime and
+    propagation length.
+
+    Models the magnon-polariton in a bulk ferromagnet.  Therefore
+    only one angle (`phi`) is sufficient to describe the propagation 
+    characteristics within this system.  However, the FMR frequency
+    is calculated for a thin film. ???? ### clarify the model
 
     Parameters
     ----------
     Bext : float
         (T) external magnetic field.
     material : Material
-        instance of `Material` describing the magnetic layer material.
-    d : float
-        (m) layer thickness (in z direction).
-    kxi : float or ndarray, default np.linspace(1e-12, 25e6, 200)
+        Instance of `Material` describing the magnetic layer material.
+        Its properties are saved as attributes, but this object is not.
+    epsilon : float
+        () real part of the dielectric constant of the material,
+        ideally at frequencies close to ferromagnetic resonance.
+    kxi : float or ndarray, optional
         (rad/m) k-vector (wavenumber), usually a vector.
-    theta : float, default np.pi/2
-        (rad) out of plane angle, pi/2 is totally inplane
-        magnetization.
-    phi : float or ndarray, default np.pi/2
-        (rad) in-plane angle, pi/2 is DE geometry.
-    weff : float, optional
-        (m) effective width of the waveguide (not used for zeroth
-        order width modes).
-    boundary_cond : {1, 2, 3, 4}, default 1
-        boundary conditions (BCs), 1 is totally unpinned and 2 is
-        totally pinned BC, 3 is a long wave limit, 4 is partially
-        pinned BC.
-    dp : float, optional
-        (rad/m) pinning parameter for 4 BC, ranges from 0 to inf,
-        0 means totally unpinned.
+    phi : float or ndarray, optional
+        (rad) angle between external field and propagation direction.
+        ### in-plane angle? (see problems above)
+    
 
     Attributes (same as Parameters, plus these)
     -------------------------------------------
@@ -43,8 +42,6 @@ class BulkPolariton:
         (A/m) saturation magnetization.
     gamma : float
         (rad*Hz/T) gyromagnetic ratio (positive convention).
-    Aex : float
-        (J/m) exchange stiffness constant.
     alpha : float
         () Gilbert damping.
     mu0dH0 : float
@@ -55,46 +52,36 @@ class BulkPolariton:
     wM : float
         (rad*Hz) parameter in Slavin-Kalinikos equation.
         `w0 = MU0*gamma*Ms`
-    A : float
-        (m^2) parameter in Slavin-Kalinikos equation.
-        `A = Aex*2/(Ms**2*MU0)`
 
     Methods
     -------
-    # ### sort these and check completeness
-    GetPartiallyPinnedKappa
     GetDisperison
     GetGroupVelocity
-    GetLifetime
+    GetLifetime  
+    # ### does it make sense to define lifetime for a polariton?
     GetDecLen
-    GetSecondPerturbation
     GetDensityOfStates
-    GetExchangeLen
-    GetEllipticity
-    GetCouplingParam
-    GetThresholdField
-
-    Private methods
-    ---------------
-    __GetAk
-    __GetBk
-
+    GetBlochFunction
+    
     Code example
     ------------
+    Example of calculation of the dispersion relation `f(k_xi)`, and
+    other important quantities, for a magnon-polariton in a bulk YIG.
     .. code-block:: python
-        # Here is an example of code
-        kxi = np.linspace(1e-12, 150e6, 150)
+        kxi = np.linspace(1e-6, 1e3, 101)
 
-        NiFeChar = SingleLayer(kxi=kxi, theta=np.pi/2, phi=np.pi/2,
-                               n=0, d=30e-9, weff=2e-6, nT=0,
-                               boundary_cond=2, Bext=20e-3, material=SWT.NiFe)
-        DispPy = NiFeChar.GetDispersion()*1e-9/(2*np.pi)  # GHz
-        vgPy = NiFeChar.GetGroupVelocity()*1e-3  # km/s
-        lifetimePy = NiFeChar.GetLifetime()*1e9  # ns
-        decLen = NiFeChar.GetDecLen()*1e6  # um
+        YIGchar = BulkPolariton(Bext=20e-3, material=SWT.YIG, 
+                                epsilon=3.0, kxi=kxi, phi=np.pi/2)
+        disp = YIGchar.GetDispersion()*1e-9/(2*np.pi)  # GHz
+        vg = YIGchar.GetGroupVelocity()*1e-3  # km/s
+        lifetime = YIGchar.GetLifetime()*1e9  # ns
+        decLen = YIGchar.GetDecLen()*1e6  # um
+
+    See also
+    --------
+    SingleLayer, SingleLayerNumeric, Material
 
     # ### update when finished adding/removing code
-    # ### add 'See also' section
     """
 
     def __init__(
@@ -102,7 +89,7 @@ class BulkPolariton:
         Bext,
         material,
         epsilon,
-        kxi=np.linspace(1e-12, 25e6, 200),
+        kxi=np.linspace(1e-6, 1e3, 200),
         phi=np.pi / 2,
     ):
         self._Bext = Bext
@@ -114,10 +101,9 @@ class BulkPolariton:
         self.phi = phi
         self.alpha = material.alpha
         self.mu0dH0 = material.mu0dH0
-        # Compute Slavin-Kalinikos parameters wM, w0, A
+        # Compute Slavin-Kalinikos parameters wM, w0
         self.wM = self.Ms * self.gamma * MU0
         self.w0 = self.gamma * self.Bext
-        self.A = self.Aex * 2 / (self.Ms**2 * MU0)
 
     @property
     def Bext(self):
@@ -160,30 +146,17 @@ class BulkPolariton:
         self.wM = self.Ms * val * MU0
         self.w0 = val * self.Bext
 
-    @property
-    def Aex(self):
-        """Exchange stiffness constant (J/m)."""
-        return self._Aex
-
-    @Aex.setter
-    def Aex(self, val):
-        self._Aex = val
-        self.A = val * 2 / (self.Ms**2 * MU0)
     def GetDispersion(self):
-        """Gives frequencies for defined k (Dispersion relation).
-        The returned values are in rad*Hz.
+        """Gives frequencies for defined k (Dispersion relation) for 
+        both hybridized modes.  The returned values are in rad*Hz.
 
-        Parameters
-        ----------
-        n : int
-            Quantization number.
-        nc : int, optional
-            Second quantization number, used for hybridization.
-        nT : int, optional
-            Waveguide (transversal) quantization number.
+        Returns
+        -------
+        w : ndarray
+            (rad*Hz) frequencies of the two hybridized modes.  Has shape
+            `(2, kxi.shape[0])`, where the first is the mode index.
         """
-        from scipy.optimize import least_squares
-        w = np.zeros((2,np.size(self.kxi, 0)))
+        w = np.zeros((2, np.size(self.kxi, 0)), dtype=np.float64)
         for idx, k in enumerate(self.kxi):
            def f(w):
                val = (self.w0 - 1/2*(2*self.epsilon*w**2/C**2 - (k*np.cos(self.phi))**2)/(k**2 - self.epsilon*w**2/C**2)*self.wM)**2 - w**2 - (1/2*(k*np.cos(self.phi))**2/(k**2 - self.epsilon*w**2/C**2)*self.wM)**2
@@ -202,29 +175,21 @@ class BulkPolariton:
         return w
     
     def GetGroupVelocity(self):
-        """Gives (tangential) group velocities for defined k.
-        The group velocity is computed as vg = dw/dk.
+        """Gives (tangential) group velocities for defined k and both 
+        hybridized modes.  The group velocity is computed as vg = dw/dk.
         The result is given in m/s.
 
         .. warning::
             Works only when `kxi.shape[0] >= 2`.
 
-        Parameters
-        ----------
-        n : int
-            Quantization number.
-        nc : int, optional
-            Second quantization number, used for hybridization.
-        nT : int, optional
-            Waveguide (transversal) quantization number.
-
         Returns
         -------
         vg : ndarray
-            (m/s) tangential group velocity.
+            (m/s) tangential group velocity.  Has shape `(2, kxi.shape[0])`,
+            where the first is the mode index.
         """
         f = self.GetDispersion()
-        vg = np.zeros((2,np.size(self.kxi, 0)))
+        vg = np.empty_like(f)
         for idm, fM in enumerate(f):
             vg[idm,:] = np.gradient(fM) / np.gradient(self.kxi)
         return vg
