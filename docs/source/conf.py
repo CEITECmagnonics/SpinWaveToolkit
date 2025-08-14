99b84a42c4672b3608fd17d4337aff81bf7b270c
Jan Klíma <66024251+GiovanniKl@users.noreply.github.com>
2025-08-14 14:37:52 +0200

    update on documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,22 +7,53 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'SpinWaveToolkit'
-copyright = '2025, Ondřej Wojewoda'
+copyright = '2025, CEITECmagnonics and contributors'
 author = 'Ondřej Wojewoda'
 release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "numpydoc",
+    "sphinx.ext.apidoc",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.autosummary",
+    "nbsphinx",
+]
 
 templates_path = ['_templates']
-exclude_patterns = []
+exclude_patterns = ["_build"]
 
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+html_theme_options = {
+    "header_links_before_dropdown": 4,
+    "logo": {
+        "image_light": "logo_light.png",
+        "image_dark": "logo_dark.png",
+    },
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/CEITECmagnonics/SpinWaveToolkit",
+            "icon": "fa-brands fa-github",
+            "type": "fontawesome",
+        },
+        {
+            "name": "PyPI",
+            "url": "https://pypi.org/project/SpinWaveToolkit/",
+            "icon": "fa-brands fa-python",
+            "type": "fontawesome",
+        },
+    ],
+    "secondary_sidebar_items": ["page-toc", "sourcelink"],
+    # ### add a version switcher (see https://pydata-sphinx-theme.readthedocs.io/en/stable/user_guide/version-dropdown.html)
+
+}
+# ### add favicon (see https://www.sphinx-doc.org/en/master/usage/configuration.html#confval-html_favicon)
